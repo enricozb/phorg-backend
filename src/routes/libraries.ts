ce5e1f553a9bfe0f77d1b768f96d474852f11efe
Enrico Borba <enricozb@gmail.com>
2020-09-21 08:44:40 -1000

    receive path, Library separately on /api/libraries

--- src/routes/libraries.ts
+++ src/routes/libraries.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import * as librariesService from "../services/libraries";
+import { Library } from "../types";
 
 export const librariesRoute = Router();
 
@@ -9,6 +10,8 @@ librariesRoute.get("/", async (req, res) => {
 });
 
 librariesRoute.post("/", async (req, res) => {
-  const {id, name, path} = req.body.library;
-  res.send(await librariesService.createLibrary(id, name, path)).status(200);
+  const {id, name} = req.body.library as Library;
+  res
+    .send(await librariesService.createLibrary(id, name, req.body.path))
+    .status(200);
 });
